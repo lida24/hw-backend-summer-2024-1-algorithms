c2f4ff466b9527b39e9d1c6ec932d7649a3a06da
lida <llenkova@element-t.ru>
2024-03-26 16:42:40 +0300

    hw-1-algorithms done

--- tasks/cartesian_product.py
+++ tasks/cartesian_product.py
@@ -8,4 +8,4 @@ T2 = TypeVar("T2")
 
 
 def cartesian_product(arr1: list[T1], arr2: list[T2]) -> list[tuple[T1, T2]]:
-    return list((a, b) for a in arr1 for b in arr2)
+    return list((el1, el2) for el1 in arr1 for el2 in arr2)
